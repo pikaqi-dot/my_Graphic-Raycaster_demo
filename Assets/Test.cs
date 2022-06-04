21423bdb8e36c22e1aa16ec0469a61c3b8419090
pikaqi-dot <hual83129@gmail.com>
2022-06-04 19:23:31 +0800

    三个场景Demo

---
+++ Assets/Test.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+	[SerializeField] private Text m_Text;
+	[SerializeField] private Button m_AddBtn;
+	[SerializeField] private Button m_RestBtn;
+
+	/// <summary>
+	/// 记录点击次数
+	/// </summary>
+	private int mClickCount;
+
+	private void Start()
+	{
+		m_AddBtn.onClick.AddListener(OnAddButtonClick);
+		m_RestBtn.onClick.AddListener(OnButtonResetClick);
+	}
+
+	private void OnButtonResetClick()
+	{
+		mClickCount = 0;
+		SetText();
+	}
+
+	private void OnAddButtonClick()
+	{
+		mClickCount++;
+		SetText();
+	}
+
+	private void SetText()
+	{
+		m_Text.text = string.Format("已点击{0}次", mClickCount);
+	}
+}
